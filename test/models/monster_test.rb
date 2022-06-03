f1bb866926993f85458a87a38714f56e421b3072
mario <github@lxxxvi.ch>
2022-06-03 09:04:54 +0200

    add BMI for monsters

--- test/models/monster_test.rb
+++ test/models/monster_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class MonsterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # height / weight * 100
+  test "body monster index" do
+    monster = Monster.new(name: "Mario", color: "Darkgray", height: 180, weight: 900)
+
+    assert_equal 20.0, monster.bmi
+  end
 end
